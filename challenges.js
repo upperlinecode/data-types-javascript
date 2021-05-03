4b07cc6f7e37535e1023aee5abfed257b664521f
Jeff <jolson615@gmail.com>
2021-05-03 17:35:06 -0400

    Update challenges.js

--- challenges.js
+++ challenges.js
@@ -1,26 +1,35 @@
 // Do either you or your partner have a pet? If so, you'll be describing them. Otherwise, have fun inventing an animal together.
 
 
-// For each of the following questions, store the answer in a variable. Then, fill in the blank in the comment with the data type you've used.
+// For each of the following questions, store the answer in a variable. 
+// Then, fill in the blank in the comment with the data type you've used.
+// Finally, console.log a message using that variable.
 
-// What is the pet's name?
+// === EXAMPLE ===
+// 0. How many times does your pet eat each day?
+const meals = 2;
+// The data type is __number__.
+console.log("My pet eats " + meals + " meals a day.");
+
+
+// 1. What is the pet's name?
 // The data type is ___.
 // const ___ = ___
 
 
-// How old is the pet?
+// 2. How old is the pet?
 // The data type is ___.
 
 
-// What type of animal is the pet?
+// 3. What type of animal is the pet?
 // The data type is ___.
 
 
-// Is the pet female?
+// 4. Is the pet female?
 // The data type is ___.
 
 
-// Approximately how much do you think the pet weighs?
+// 5. Approximately how much do you think the pet weighs?
 // The data type is ___.
 
 
