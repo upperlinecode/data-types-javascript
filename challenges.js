1ea00453489070487d44e4809f54e4e7467cb537
Aaron <aaronpfink@gmail.com>
2021-05-03 16:11:34 -0400

    Create challenges.js

---
+++ challenges.js
@@ -0,0 +1,35 @@
+// Do either you or your partner have a pet? If so, you'll be describing them. Otherwise, have fun inventing an animal together.
+
+
+// For each of the following questions, store the answer in a variable. Then, fill in the blank in the comment with the data type you've used.
+
+// What is the pet's name?
+// The data type is ___.
+// const ___ = ___
+
+
+// How old is the pet?
+// The data type is ___.
+
+
+// What type of animal is the pet?
+// The data type is ___.
+
+
+// Is the pet female?
+// The data type is ___.
+
+
+// Approximately how much do you think the pet weighs?
+// The data type is ___.
+
+
+
+
+// You shouldn't need it often, but JavaScript has a neat built-in function called "typeof" which can tell you the data type stored in a variable.
+// For example, if you call typeof("koala"), JavaScript will give you "string."
+
+
+// Check your answers now with five call to console.log, which print statements of the form "Penelope is a string."
+
+// console.log(___ + " is a " + typeof())
